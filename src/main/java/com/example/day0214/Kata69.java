8fa7da2ac157318baeb77d6f221c7abe778e35e0
queenriwon <queenriwon3@gmail.com>
2025-02-17 09:51:53 +0900

    Todo: 250217 - Kata69

--- src/main/java/com/example/day0214/Kata69.java
+++ src/main/java/com/example/day0214/Kata69.java
@@ -3,6 +3,13 @@ package com.example.day0214;
 // https://school.programmers.co.kr/learn/courses/30/lessons/118666
 
 /*
+1번 지표	라이언형(R), 튜브형(T)
+2번 지표	콘형(C), 프로도형(F)
+3번 지표	제이지형(J), 무지형(M)
+4번 지표	어피치형(A), 네오형(N)
+ */
+
+/*
 ["AN", "CF", "MJ", "RT", "NA"]	[5, 3, 2, 7, 5]	"TCMA"
 ["TR", "RT", "TR"]	[7, 1, 3]	"RCJA"
  */
@@ -18,7 +25,7 @@ public class Kata69 {
 
 class Solution69 {
     public String solution(String[] survey, int[] choices) {
-        String answer = "";
-        return answer;
+        // ...
+        return null;
     }
 }
